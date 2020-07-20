78c0991df535bc8294f1408188d2f49b7c6cd0be
moussagerges <moussagerges@users.noreply.github.com>
2020-07-20 22:03:09 +0200

    Caesar-Cipher

---
+++ Caesar cipher/Caesar cipher/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Caesar_cipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.StButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(245, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 32);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Caesar Cipher";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            " ",
+            "Encryption",
+            "Decryption"});
+            this.comboBox1.Location = new System.Drawing.Point(447, 205);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 33);
+            this.comboBox1.TabIndex = 25;
+            // 
+            // StButton
+            // 
+            this.StButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StButton.Location = new System.Drawing.Point(278, 256);
+            this.StButton.Name = "StButton";
+            this.StButton.Size = new System.Drawing.Size(115, 47);
+            this.StButton.TabIndex = 24;
+            this.StButton.Text = "Start";
+            this.StButton.UseVisualStyleBackColor = true;
+            this.StButton.Click += new System.EventHandler(this.StButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultTextBox.Location = new System.Drawing.Point(51, 142);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(568, 34);
+            this.resultTextBox.TabIndex = 23;
+            this.resultTextBox.Text = "Result";
+            this.resultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.ForeColor = System.Drawing.Color.Black;
+            this.inputTextBox.Location = new System.Drawing.Point(51, 71);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(568, 34);
+            this.inputTextBox.TabIndex = 22;
+            this.inputTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputTextBox_KeyPress);
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyTextBox.ForeColor = System.Drawing.Color.Black;
+            this.keyTextBox.Location = new System.Drawing.Point(51, 205);
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(138, 34);
+            this.keyTextBox.TabIndex = 27;
+            this.keyTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.keyTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyTextBox_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "key:";
+            // 
+            // copyButton
+            // 
+            this.copyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.copyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copyButton.Location = new System.Drawing.Point(630, 144);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(65, 31);
+            this.copyButton.TabIndex = 29;
+            this.copyButton.Text = "Copy";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 330);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.keyTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.StButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Caesar cipher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button StButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button copyButton;
+    }
+}
+
---
+++ Caesar cipher/Caesar cipher/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caesar_cipher
+{
+    public partial class Form1 : Form
+    {
+        private int _key;
+        string _sentence;
+        private string _result;
+        private int _plus;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StButton_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Length > 0 && !String.IsNullOrWhiteSpace(inputTextBox.Text) &&
+                keyTextBox.Text.Length > 0) 
+            {
+                _key= int.Parse(keyTextBox.Text);
+                _sentence = inputTextBox.Text.ToLower();
+                _result = "";
+                switch (comboBox1.Text)
+                {
+                    case "Encryption":
+                        Encryption();
+                        break;
+                    
+                    case "Decryption":
+                        Decryption();
+                        break;
+                }
+            }
+        }
+
+
+        private void Encryption()
+        {
+            foreach (var x in _sentence)
+            {
+                    _plus = x + _key;
+                    if (_plus > 122)
+                    {
+                        _result +=  (char) ((char)97 + ((char) _plus - 123));
+                    }
+                    else
+                        _result += (char)(x + _key);
+            }
+
+            resultTextBox.Text = _result;
+        }
+
+        private void Decryption()
+        {
+            foreach (var x in _sentence)
+            {
+                    _plus = x - _key;
+                    if (_plus < 97)
+                    {
+                        int dKey = _key - (x - 96);
+                        _result += (char) ((char)122 - dKey);
+                    }
+                    else
+                        _result +=(char) (x - _key);
+            }
+            resultTextBox.Text = _result;
+        }
+
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(resultTextBox.Text);
+        }
+
+        private void keyTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void inputTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back)
+                 e.Handled =false ;
+            else
+                e.Handled = true;
+        }
+    }
+}
